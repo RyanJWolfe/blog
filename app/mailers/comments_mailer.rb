f3a33f1e65a45ed89d7a21edad4f4b71689b8ccd
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-01 02:07:12 -0700

    Add test gems, run rubocop autocorrect

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -1,13 +1,14 @@
-class CommentsMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class CommentsMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.comments_mailer.submitted.subject
   #
-  def submitted(comment)
-    @comment = "comment"
+  def submitted(_comment)
+    @comment = 'comment'
 
-    mail to: "blog-owner@example.org", subject: "New comment!"
+    mail to: 'blog-owner@example.org', subject: 'New comment!'
   end
 end
--- test/mailers/comments_mailer_test.rb
+++ test/mailers/comments_mailer_test.rb
@@ -1,12 +1,13 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class CommentsMailerTest < ActionMailer::TestCase
-  test "submitted" do
+  test 'submitted' do
     mail = CommentsMailer.submitted
-    assert_equal "Submitted", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Submitted', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['from@example.com'], mail.from
+    assert_match 'Hi', mail.body.encoded
   end
-
 end
