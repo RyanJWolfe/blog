a330b0b81ecf67d363eb80210aff6f6734f60690
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-03 00:27:35 -0700

    Only show new post button for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Admin.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 10.times do
   Post.create!(title: Faker::Lorem.sentence)
