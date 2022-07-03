d03d7edfec3c64f82d84861bce0446ad9a9d369d
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-03 00:06:40 -0700

    Authenticate posts CRUD actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Admin.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+post1 = Post.create!(title: 'First Post', content: 'This is the first post')
+post2 = Post.create!(title: 'Second Post', content: 'This is the second post')
+Post.create!(title: 'Third Post', content: 'This is the third post')
+
+post1.comments.create!(content: 'This is the first comment')
+post1.comments.create!(content: 'This is the second comment')
+
+post2.comments.create!(content: 'This is the first comment')
