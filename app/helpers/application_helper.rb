46994e5e033bbe84b716d36f5db3401d8bc8e298
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-03 01:36:38 -0700

    Partials for nav and side menu links, generate pages controller w index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def inside_posts
+    %w[posts].include? controller_name
+  end
+
+  def inside_about
+    %w[about].include? controller_name
+  end
+
+  def inside_contact
+    %w[contact].include? controller_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'posts#index'
+  root 'pages#index'
 
   devise_for :admins
 
