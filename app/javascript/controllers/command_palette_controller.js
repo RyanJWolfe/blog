25d60554915823aa79e59dbc2fb789672ce4d7c2
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-03 18:07:05 -0700

    Pin ninja-keys via cdn

---
+++ app/javascript/controllers/command_palette_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import "ninja-keys"
+
+// Connects to data-controller="command-palette"
+export default class extends Controller {
+  connect() {
+  }
+}
