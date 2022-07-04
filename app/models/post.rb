1f884a22c99a54b27f9c60808882f1407c8191f7
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-07-03 18:59:27 -0700

    Install pg-search and include in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  include PgSearch::Model
+  # pg_search_scope :search_by_title_and_content, against: [:title, :content]
+
   validates_presence_of :title
   has_rich_text :content
   has_many :comments, dependent: :destroy
